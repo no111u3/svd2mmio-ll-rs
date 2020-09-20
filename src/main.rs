1e650b13eb5904691161b4c60b4452b8f2fe917d
Boris Vinogradov <no111u3@gmail.com>
2020-09-20 17:53:50 +0300

    Add base structure to main for getting parse file name and read it

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,60 @@ use svd2mmio_ll_rs::opt::Opt;
 
 use structopt::StructOpt;
 
+use std::fs::File;
+use std::io::Read;
+use std::process;
+
+use log::{error, info};
+
+use svd_parser as svd;
+
+use anyhow::{Context, Result};
+
+fn run() -> Result<()> {
+    let opt = Opt::from_args();
+
+    setup_logging();
+
+    let input = opt.input;
+
+    info!("input: {:?}", input);
+
+    let svd_xml = &mut String::new();
+    File::open(input)
+        .context("Cannot open the SVD file")?
+        .read_to_string(svd_xml)
+        .context("Cannot read the SVD file")?;
+
+    let device = svd::parse(svd_xml)?;
+
+    info!("device: {}", device.name);
+
+    Ok(())
+}
+
+fn setup_logging() {
+    let env = env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info");
+    let mut builder = env_logger::Builder::from_env(env);
+    builder.format_timestamp(None);
+
+    let log_lvl_from_env = std::env::var_os(env_logger::DEFAULT_FILTER_ENV).is_some();
+
+    if log_lvl_from_env {
+        log::set_max_level(log::LevelFilter::Trace);
+    } else {
+        let level = log::LevelFilter::Info;
+        log::set_max_level(level);
+        builder.filter_level(level);
+    }
+
+    builder.init();
+}
+
 fn main() {
-    let _opt = Opt::from_args();
+    if let Err(ref e) = run() {
+        error!("{:?}", e);
+
+        process::exit(1);
+    }
 }
--- src/opt.rs
+++ src/opt.rs
@@ -3,6 +3,12 @@
 //! Command line options as StructOpt derive structure
 use structopt::StructOpt;
 
+use std::path::PathBuf;
+
 #[derive(StructOpt, Debug)]
 #[structopt(name = env!("CARGO_PKG_NAME"), about = env!("CARGO_PKG_DESCRIPTION"))]
-pub struct Opt {}
+pub struct Opt {
+    /// Input SVD file path
+    #[structopt(short, long, parse(from_os_str))]
+    pub input: PathBuf,
+}
